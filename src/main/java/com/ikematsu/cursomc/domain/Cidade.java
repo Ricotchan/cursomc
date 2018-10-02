c278a029b61034657c71fe779b42edae271ef135
Ikematsu <ricotchan@gmail.com>
2018-10-02 15:53:36 -0300

    Cidade e Estado

--- src/main/java/com/ikematsu/cursomc/domain/Cidade.java
+++ src/main/java/com/ikematsu/cursomc/domain/Cidade.java
@@ -1,39 +1,35 @@
 package com.ikematsu.cursomc.domain;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
-public class Categoria implements Serializable{
+public class Cidade implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
-	//FAZENDO O RELACIONAMETO, COMO JÁ FOI FEITO NA CLASSE 
-	//PRODUTO, NÃO HÁ NECESSIDADE DE REPETIR O CÓDIGO, APENAS ASSOCIAR.
-	@JsonManagedReference
-	@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<>();
-	
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
-	public Categoria() {
+	@ManyToOne
+	@JoinColumn(name="estado_id")
+	private Estado estado;
+	
+	public Cidade() {
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Cidade(Integer id, String nome, Estado estado) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.estado = estado;
 	}
 
 	public Integer getId() {
@@ -52,14 +48,14 @@ public class Categoria implements Serializable{
 		this.nome = nome;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public Estado getEstado() {
+		return estado;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setEstado(Estado estado) {
+		this.estado = estado;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -76,7 +72,7 @@ public class Categoria implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Cidade other = (Cidade) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -84,6 +80,6 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
-
-
+	
+	
 }
